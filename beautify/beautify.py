ca0cd40d483e63fed0af7d86427bf90ee11187d5
mehan <mehan.alavi.88.majd@gmail.com>
2024-06-27 18:09:55 +0330

    change of bools in the data

--- beautify/beautify.py
+++ beautify/beautify.py
@@ -5,13 +5,16 @@ pre-pre-processing the data in data_raw.csv and changing all persian numbers to 
 
 import pandas as pd
 import numpy as np
-from utils import persianNumber,floorNumber
-df = pd.read_csv('scrape/data_raw.csv')
-
+from utils import persianNumber,floorNumber,boolHandler
+df = pd.read_csv('./data_raw.csv')
+print(df)
 df['price'] = df['price'].map(persianNumber)
 df['rooms'] = df['rooms'].map(persianNumber)
 df['meter'] = df['meter'].map(persianNumber)
 df['buildYear'] = df['buildYear'].map(persianNumber)
 df['floor'] = df['floor'].map(floorNumber)
+df['elevator'] = df['elevator'].map(boolHandler)
+df['space'] = df['space'].map(boolHandler)
+df['parking'] = df['parking'].map(boolHandler)
 df=df.drop_duplicates()
 df.to_csv("data.csv",index=False)
