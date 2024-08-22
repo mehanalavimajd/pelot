027560baae74b06766c49ee78c3fc2786257c913
mehan <mehan.alavi.88.majd@gmail.com>
2024-08-22 23:50:54 +0330

    district option

--- handler/districts.py
+++ handler/districts.py
@@ -1,8 +1,11 @@
 import pandas as pd
-df=pd.read_csv("data.csv")
+df=pd.read_csv("data2.csv")
 districts=[]# name: repeat count
 for index,row in df.iterrows():
     if(not row['district'] in districts):
         districts.append(row['district'])
-        
+districts=sorted(districts)
 print(districts)
+with open("district.txt",'w') as f:
+    for i in districts:
+        f.write(f'<option value="{i}">{i}</option>\n')
